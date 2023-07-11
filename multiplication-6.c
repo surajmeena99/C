d2a8eca6e70561408c7c1e25fefd53ae3ec00c6b
surajmeena99 <suraj1181999@gmail.com>
2023-07-11 22:19:30 +0530

    multiplication

---
+++ multiplication-6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int num1, num2, multiplication;
+    printf("enter first number:");
+    scanf("%d", &num1);
+    printf("enter second number:");
+    scanf("%d", &num2);
+    multiplication = num1 * num2;
+    // int multiplication = num1 * num2;
+    // printf("%d * %d =%d",num1,num2, multiplication);
+    printf("%d", multiplication);
+    return 0;
+}
