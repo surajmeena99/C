b14a71a986c3dd4d27a2397380e3fe06fb015e27
surajmeena99 <suraj1181999@gmail.com>
2023-08-03 23:38:07 +0530

    check eligiblity

---
+++ if-else/eligiblity.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int age;
+    printf("enter your age:");
+    scanf("%d", &age);
+   
+    if(age >= 18){
+        printf("you are eligible");
+    }
+    else{
+        printf("you are not eligible");
+    }
+    return 0;
+}
