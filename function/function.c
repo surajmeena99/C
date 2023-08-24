214203ae0ba51a0fda7ea6ef1dd94e20c6772a5e
surajmeena99 <suraj1181999@gmail.com>
2023-08-24 10:40:09 +0530

    add function

---
+++ function/function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void greetings()  //Function declaration
+{
+    printf("\n Hello and welcome to programming");
+    printf("\n We are developers");
+}
+main()
+{
+    greetings();   //Function calling
+    greetings();
+    // greetings();
+}
+
+
